cec2b2367ad6e6e301deacc8108331a3af577d62
cc8carat <chloe26.ni@gmail.com>
2022-01-03 15:38:33 +0100

    first commit

---
+++ src/components/Signup.tsx
@@ -0,0 +1,11 @@
+import { IonPage } from '@ionic/react';
+
+const Signup: React.FC = () => {
+  return (
+    <IonPage>
+      <h2>haha</h2>
+    </IonPage>
+  );
+};
+
+export default Signup;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -11,12 +11,11 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader collapse='condense'>
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size='large'>Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>
   );
